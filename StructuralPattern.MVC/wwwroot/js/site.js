74b187e0db7052fbf3016fe9932e8b53ac569e01
Radoslav K <rkezunovski98@gmail.com>
2023-01-12 22:59:53 +0100

    html & js / ui

--- StructuralPattern.MVC/wwwroot/js/site.js
+++ StructuralPattern.MVC/wwwroot/js/site.js
@@ -1,4 +1,16 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
+﻿let currentSelectedElement = null;
+let mainTextArea = document.querySelector('#main-text');
+let imgSelectElements = document.querySelectorAll('.img-select');
 
-// Write your JavaScript code.
+imgSelectElements.forEach(el => {
+    el.addEventListener('click', (e) => {
+        mainTextArea.id = el.id;
+        
+        if(currentSelectedElement != null){
+            currentSelectedElement.classList.remove('selected-border');
+        }
+        
+        currentSelectedElement = el;
+        currentSelectedElement.classList.add('selected-border');
+    })
+})
